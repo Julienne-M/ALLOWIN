9fa7b94b2e8ddce1578d2224a76e8e2a89dceeec
Julienne Matias <julienne.m93@gmail.com>
2018-10-30 16:10:40 +0100

    Création composants Enfant et Adulte, mise en place liens app

---
+++ src/pages/childForm/childForm.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-childForm',
+    templateUrl: 'childForm.html'
+})
+export class ChildFormPage {
+    
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ChildFormPage } from '../childForm/childForm';
+import { AdultFormPage } from '../adultForm/adultForm';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToChildForm() {
+    this.navCtrl.push(ChildFormPage)
+  }
+  
+  onGoToAdultForm() {
+    this.navCtrl.push(AdultFormPage)
   }
 
 }
